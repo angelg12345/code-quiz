d4b4c09709e5292602bf9a5ec43656874a345958
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-04-09 11:32:48 -0700

    Adding questions in a let array into javascript. added comments into all 3 files to help other programmers read my code.

--- script.js
+++ script.js
@@ -2,8 +2,53 @@
 var startQuiz = document.getElementById("start-quiz")
 var timeEl = document.querySelector(".timer")
 
-var secondsleft = 30;
+
+let questions = [
+    {
+        numb: 1,
+        question: "What html tag is used to connnect the html to the javascript page?",
+        answer: "<script><script/>",
+        options: [
+            "<link><link/>",
+            "<java><java/>",
+            "<src><src/>",
+            "<script><script/>"
+        ]
+    },
+    {
+        numb: 2,
+        question:
+        answer:
+        options: [
+            
+        ]
+    }
+        
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var secondsleft = 60;
+
 startQuiz.addEventListener("click", function(){
+// This is the timer
     var timerInterval = setInterval(function(){
         secondsleft--;
         timeEl.textContent = secondsleft + " seconds left "
@@ -12,7 +57,6 @@ startQuiz.addEventListener("click", function(){
             clearInterval(timerInterval);
         }
     }, 1000)
-    var question = document.getElementById("question");
-    question.innerHTML = "Whats your last name?";
+//this is the question change that happens
 });
 startQuiz()
